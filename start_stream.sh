fd0636cad794578eda65913af21357f83a359c9f
Henry Dalrymple <114787559+HenryDalrymple53@users.noreply.github.com>
2026-02-19 12:08:11 -0800

    Modify video streaming command for lower framerate

--- start_stream.sh
+++ start_stream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true;
 do
-    gst-launch-1.0 libcamerasrc ! video/x-raw,width=4608,height=2592 ! videoconvert ! x264enc bitrate=50000 tune=zerolatency ! h264parse config-interval=1 ! mpegtsmux ! filesink location=video.ts
+    gst-launch-1.0 libcamerasrc ! video/x-raw,width=4608,height=2592,framerate=1/5 ! videoconvert ! queue ! x264enc bitrate=50000 tune=zerolatency key-int-max=1 ! queue ! h264parse config-interval=1 ! mpegtsmux ! queue ! filesink location=video.ts sync=true
     sleep 1
     wait
 done
